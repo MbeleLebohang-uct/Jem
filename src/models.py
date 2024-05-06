59b98b22be625bb861cce98ccc38f425fbd8e28b
TatendaMugadza <tatenda@reachdigitalhealth.org>
2024-05-06 22:56:29 +0200

    added seed script

--- src/models.py
+++ src/models.py
@@ -77,3 +77,20 @@ class User:
     async def create(cls, **user):
         query = users.insert().values(**user)
         return await database.execute(query)
+    
+    
+class Organization:
+    @classmethod
+    async def all(cls):
+        query = organizations.select()
+        return await database.fetch_all(query)
+    
+    @classmethod
+    async def get(cls, id):
+        query = organizations.select().where(organizations.c.id == id)
+        return await database.fetch_one(query)
+
+    @classmethod
+    async def create(cls, **organization):
+        query = organizations.insert().values(**organization)
+        return await database.execute(query)
